1772e19510422e8f75e0210cd90914deb96d2ece
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-04-11 17:52:04 +0100

    created an install route

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ app.get("/", (req,res)=> {
   res.send("Hello World");
 });
 
+app.get("/shopify",(req,res)=> {
+  const shop = req.query.shop;
+  if(shop) {
+    const state = nonce();
+    const redirectUri = `${forwardingAddress}/shopify/callback`;
+    const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${apiKey}&scope=${scopes}&state=${state}&redirect_uri=${redirectUri}`;
+    res.cookie('state', state);
+    return res.redirect(installUrl);
+  }
+  return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+})
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
 });
